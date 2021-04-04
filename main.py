dd0d986d3253803a6e65164b64e079e13eb7ae29
Tony Lin <>
2021-04-04 02:42:15 +0000

    added customprefix

--- main.py
+++ main.py
@@ -12,13 +12,51 @@ from discord.ext import commands, tasks
 from discord.ext.commands import has_permissions,  CheckFailure, check
 #^ basic imports for other features of discord.py and python ^
 
-client = discord.Client()
 
-def get_prefix(client,message):
+
+def get_prefix(client, message):
   with open("prefixes.json", "r") as f:
     prefixes = json.load(f)
+
   return prefixes[str(message.guild.id)]
-client = commands.Bot(command_prefix = ("m"))
+  
+
+client = commands.Bot(command_prefix = get_prefix)
+@client.event
+async def on_guild_join(guild):
+  with open("prefixes.json", "r") as f:
+    prefixes = json.load(f)
+  prefixes[str(guild.id)] = "m"
+  with open("prefixes.json", "w") as f:
+    json.dump(prefixes,f, indent=4)
+
+
+
+
+
+@client.event
+async def on_guild_remove(guild):
+    with open('prefixes.json', 'r') as f:
+        prefixes = json.load(f)
+ 
+    prefixes.pop(str(guild.id))
+ 
+    with open('prefixes.json', 'w') as f:
+        json.dump(prefixes, f, indent=4)
+
+@client.command()
+async def setprefix(ctx, prefix):
+    with open('prefixes.json', 'r') as f:
+        prefixes = json.load(f)
+ 
+    prefixes[str(ctx.guild.id)] = prefix
+ 
+    with open('prefixes.json', 'w') as f:
+        json.dump(prefixes, f, indent=  4)
+ 
+    await ctx.send(f'The prefix is now: **``{prefix}``**')
+
+
 
 @client.event
 async def on_ready():
@@ -440,10 +478,10 @@ client.remove_command('help')
 @client.command()
 async def help(ctx):
   embed=discord.Embed(title="OwObot Helper", color=0x70ffee)
-  embed.add_field(name="Ping", value="Gets bot latency", inline=False)
-  embed.add_field(name="Info", value="Shows bot info", inline=False)
-  embed.add_field(name="Choose", value="mchoose {a} {b}, chooses random choice", inline=False)
-  embed.add_field(name="cf/coinflip", value="mcf {head or tail}, will generate a coinflip", inline=False)
+  embed.add_field(name="Ping", value="Gets client latency", inline=False)
+  embed.add_field(name="Info", value="Shows client info", inline=False)
+  embed.add_field(name="Choose", value="{prefix}choose {a} {b}, chooses random choice", inline=False)
+  embed.add_field(name="cf/coinflip", value="{prefix}cf {head or tail}, will generate a coinflip", inline=False)
   embed.set_footer(text="coop is cute")
   await ctx.send(embed=embed)
 
@@ -458,26 +496,29 @@ async def choose(ctx, *choices: str):
 
 @client.command()  
 async def cf(ctx, headtail: str):
-  if(headtail != "head") and headtail != "tail":
-    headtail = random.choice("head", "tail")
-  else:
-    
-    choices = ["head","tail"]
-    a = random.choice(choices)
-    
-    if a == headtail:
-      embed=discord.Embed(title="Coinflip! ", description=("the coin landed on " + a + " you good good!!!"), color=0x00ff08)
-      embed.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
-      embed.set_thumbnail(url="https://gifimage.net/wp-content/uploads/2017/10/coin-flip-gif-3.gif")
-      embed.set_footer(text="coopw is cute")
-      await ctx.send(embed=embed)
-
+  try:
+    if(headtail != "head") and headtail != "tail":
+      headtail = random.choice("head", "tail")
     else:
-      embed=discord.Embed(title="Coinflip! ", description=("the coin landed on " + a + " you bad bad!!!"), color=0xff0000)
-      embed.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
-      embed.set_thumbnail(url="https://gifimage.net/wp-content/uploads/2017/10/coin-flip-gif-3.gif")
-      embed.set_footer(text="coopw is cute")
-      await ctx.send(embed=embed)
+    
+      choices = ["head","tail"]
+      a = random.choice(choices)
+    
+      if a == headtail:
+        embed=discord.Embed(title="Coinflip! ", description=("the coin landed on " + a + " you good good!!!"), color=0x00ff08)
+        embed.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
+        embed.set_thumbnail(url="https://gifimage.net/wp-content/uploads/2017/10/coin-flip-gif-3.gif")
+        embed.set_footer(text="coopw is cute")
+        await ctx.send(embed=embed)
+
+      else:
+        embed=discord.Embed(title="Coinflip! ", description=("the coin landed on " + a + " you bad bad!!!"), color=0xff0000)
+        embed.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
+        embed.set_thumbnail(url="https://gifimage.net/wp-content/uploads/2017/10/coin-flip-gif-3.gif")
+        embed.set_footer(text="coopw is cute")
+        await ctx.send(embed=embed)
+  except:
+    await ctx.send("error!")
 
 
 
@@ -507,19 +548,6 @@ async def coinflip(ctx, headtail: str):
       embed.set_footer(text="coopw is cute")
       await ctx.send(embed=embed)
 
-@client.command(pass_context=True)
-@has_permissions(administrator=True) 
-async def setprefix(ctx, prefix): 
-    with open('prefixes.json', 'r') as f:
-        prefixes = json.load(f)
-
-    prefixes[str(ctx.guild.id)] = prefix
-
-    with open('prefixes.json', 'w') as f: 
-        json.dump(prefixes, f, indent=4)
-
-    await ctx.send(f'Prefix changed to: {prefix}') 
-    name=f'{prefix}Auxii'
 
 
 
